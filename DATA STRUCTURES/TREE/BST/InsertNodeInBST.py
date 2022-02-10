c28370338b5bfa275ea38fee47b079431b755621
Abhi <abhishekvelekar792000@gmail.com>
2022-02-10 08:21:01 +0530

    Insertion in BST

---
+++ DATA STRUCTURES/TREE/BST/InsertNodeInBST.py
@@ -0,0 +1,23 @@
+def insert(root, Key):
+    # code here
+    
+    cur = root 
+    pre=None
+    
+    while (cur!=None):
+        if(cur.data==Key):
+            return root
+        elif(Key>cur.data):
+            pre=cur
+            cur=cur.right
+        else:
+            pre=cur
+            cur=cur.left
+        
+        
+    if(Key>pre.data):
+        pre.right=Node(Key)
+    else:
+        pre.left=Node(Key)
+    
+    return root
